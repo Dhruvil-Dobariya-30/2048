d635804023e7f19ae483476835071f6d2a5edeea
Dhruvil Dobariya <dhruvil.devoteeinfotech@gmail.com>
2024-08-02 18:16:33 +0530

    sum feature

--- index.js
+++ index.js
@@ -1,39 +1,63 @@
+let div = document.getElementById("container");
+let mainArray = [];
 let allPlace = [];
 let size = 4;
+let copyArr = [];
 
 document.onkeydown = (e) => {
   e = e || window.event;
   let arrowPressed = false;
 
   if (e.key === "ArrowUp") {
+    copyArr = structuredClone(mainArray);
+
     arrowPressed = true;
     topShift();
     topSum();
-    genarateIndex();
+    // genarateIndex();
   } else if (e.key === "ArrowDown") {
+    copyArr = structuredClone(mainArray);
+
     arrowPressed = true;
     downShift();
     downSum();
-    genarateIndex();
+    // genarateIndex();
   } else if (e.key === "ArrowLeft") {
+    copyArr = structuredClone(mainArray);
+
     arrowPressed = true;
     leftShift();
     leftSum();
-    genarateIndex();
+    // genarateIndex();
   } else if (e.key === "ArrowRight") {
+    copyArr = structuredClone(mainArray);
+
     arrowPressed = true;
     righShift();
     rightSum();
-    genarateIndex();
+    // genarateIndex();
   }
+  console.log(mainArray);
+  console.log(copyArr);
+
+  checkChanges();
 
   // if (arrowPressed) {
   //   genarateIndex();
   // }
 };
 
-let mainArray = [];
-let div = document.getElementById("container");
+let abc = false;
+function checkChanges() {
+  mainArray.map((item) => {
+    copyArr.map((value, index, array) => {
+      for (let i = 0; i < size; i++) {
+        let check = item.includes(value[i]);
+        console.log(check);
+      }
+    });
+  });
+}
 
 function displayBox() {
   let div = "";
@@ -42,12 +66,11 @@ function displayBox() {
     div += "<br/>";
     for (let j = 0; j < size; j++) {
       mainArray[i][j] = `${i}${j}`;
-      div += `<div class="box" id="${mainArray[i][j]}" onkeyup="keyHandle(this.id)"></div>`;
+      div += `<div class="box" id="${mainArray[i][j]}"></div>`;
     }
   }
 
   document.getElementById("container").innerHTML = div;
-  console.log(mainArray);
   generateTwoNums();
 }
 displayBox();
@@ -58,18 +81,24 @@ function generateTwoNums() {
 }
 
 function genarateIndex() {
-  let randomRow = Math.floor(Math.random() * size);
-  let randomCol = Math.floor(Math.random() * size);
+  // let randomRow = Math.floor(Math.random() * size);
+  // let randomCol = Math.floor(Math.random() * size);
 
-  let position = `${randomRow}${randomCol}`;
+  // try {
+  //   let position = `${randomRow}${randomCol}`;
 
-  if (!allPlace.includes(position)) {
-    allPlace.push(position);
-    genarateNumber(randomRow, randomCol);
-  } else {
-    genarateIndex();
-  }
-  console.log(allPlace);
+  //   if (!allPlace.includes(position)) {
+  //     allPlace.push(position);
+  //     genarateNumber(randomRow, randomCol);
+  //   } else {
+  //     genarateIndex();
+  //   }
+  // } catch (err) {
+  //   // alert("GAME OVER!!!");
+
+  //   document.getElementById("msg").innerHTML = "GAME OVER!!!";
+  // }
+  console.log("hello");
 }
 
 function genarateNumber(row, col) {
@@ -94,8 +123,7 @@ function rightSum() {
   for (let i = 0; i < size; i++) {
     for (let j = 0; j < size; j++) {
       righShift();
-      // if (mainArray[i][j] === 2 || mainArray[i][j] == 4) {
-      if (mainArray[i][j] == mainArray[i][j + 1]) {
+      if (mainArray[i][j] === mainArray[i][j + 1]) {
         mainArray[i][j + 1] = mainArray[i][j] + mainArray[i][j + 1];
         mainArray[i][j] = `${i}${j}`;
 
@@ -106,14 +134,12 @@ function rightSum() {
     }
   }
 }
-// }
 
 function leftSum() {
   for (let i = 0; i < size; i++) {
     for (let j = 0; j < size; j++) {
       leftShift();
-      // if (mainArray[i][j] === 2 || mainArray[i][j] == 4) {
-      if (mainArray[i][j] == mainArray[i][j - 1]) {
+      if (mainArray[i][j] === mainArray[i][j - 1]) {
         mainArray[i][j - 1] = mainArray[i][j] + mainArray[i][j - 1];
         mainArray[i][j] = `${i}${j}`;
 
@@ -124,14 +150,12 @@ function leftSum() {
     }
   }
 }
-// }
 
 function topSum() {
   for (let i = 0; i < size; i++) {
     for (let j = 0; j < size; j++) {
       topShift();
-      // if (mainArray[i][j] === 2 || mainArray[i][j] == 4) {
-      if (i > 0 && mainArray[i][j] == mainArray[i - 1][j]) {
+      if (i > 0 && mainArray[i][j] === mainArray[i - 1][j]) {
         mainArray[i - 1][j] = mainArray[i][j] + mainArray[i - 1][j];
         mainArray[i][j] = `${i}${j}`;
 
@@ -146,14 +170,14 @@ function topSum() {
 function downSum() {
   for (let i = 0; i < size; i++) {
     for (let j = 0; j < size; j++) {
-      downShift();
-      if (i < 4 && i >= 0 && mainArray[i][j] == mainArray[i + 1][j]) {
-        mainArray[i + 1][j] = mainArray[i][j] + mainArray[i + 1][j];
+      if (i < size - 1 && mainArray[i][j] === mainArray[i + 1][j]) {
+        mainArray[i + 1][j] =
+          parseInt(mainArray[i][j]) + parseInt(mainArray[i + 1][j]);
         mainArray[i][j] = `${i}${j}`;
 
         document.getElementById(`${i + 1}${j}`).innerHTML = mainArray[i + 1][j];
-        document.getElementById(mainArray[i][j]).innerHTML = "";
-        allPlace = allPlace.filter((data) => data !== mainArray[i][j]);
+        document.getElementById(`${i}${j}`).innerHTML = "";
+        allPlace = allPlace.filter((data) => data !== `${i}${j}`);
       }
     }
   }
@@ -163,16 +187,12 @@ function righShift() {
   for (let i = 0; i < size; i++) {
     for (let j = 0; j < size; j++) {
       if (mainArray[i][j] !== `${i}${j}`) {
-        if (i <= size && mainArray[i][j + 1] == `${i}${j + 1}`) {
+        if (i < size && mainArray[i][j + 1] == `${i}${j + 1}`) {
           mainArray[i][j + 1] = mainArray[i][j];
           mainArray[i][j] = `${i}${j}`;
-          console.log(mainArray);
-
           document.getElementById(`${i}${j + 1}`).innerHTML =
             mainArray[i][j + 1];
-
           document.getElementById(`${i}${j}`).innerHTML = "";
-
           allPlace.push(`${i}${j + 1}`);
           allPlace = allPlace.filter((data) => data !== mainArray[i][j]);
         }
@@ -227,7 +247,7 @@ function downShift() {
   for (let i = 0; i < size; i++) {
     for (let j = 0; j < size; j++) {
       if (mainArray[i][j] !== `${i}${j}`) {
-        if (mainArray[i + 1][j] == `${i + 1}${j}`) {
+        if (i < size - 1 && mainArray[i + 1][j] == `${i + 1}${j}`) {
           mainArray[i + 1][j] = mainArray[i][j];
           mainArray[i][j] = `${i}${j}`;
 
@@ -242,4 +262,8 @@ function downShift() {
       }
     }
   }
+}
+
+function restart() {
+  location.reload();
 }
